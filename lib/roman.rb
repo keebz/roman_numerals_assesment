4b3250852386d46eb0cea0bbe6030b56e1298419
keebz <keebz@keebz.com>
2014-07-30 13:30:29 -0700

    refactors the program to use a loop through the has of roman numerals

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,46 +1,37 @@
 def roman_numeral(number)
-	roman_conversion = {"m" => 1000, "cm" => 900, "d" => 500, "cd" => 400, "c" => 100, "xc" => 90, "l" => 50, "xl" => 40, "x" => 10, "ix" => 9, "v" => 5, "iv" => 4, "i" => 1}
+  roman_conversion = {"m" => 1000, "cm" => 900, "d" => 500, "cd" => 400, "c" => 100, "Xc" => 90, "l" => 50, "xl" => 40, "x" => 10, "Ix" => 9, "v" => 5, "iv" => 4, "i" => 1}
 
 number_array = []
 
-  roman_conversion.each do |letter, value|
-  	next_number = (number / value)
-    
-    if next_number >= 1 
-       number_array << letter * next_number
-       number = (number - value) 
+counter = 1
+
+  while counter == 1 do
+    roman_conversion.each do |letter, value|
+    if number == 1
+      counter == 0
+      break
+
+    elsif number == value
+      number_array << letter
+      counter = 0
+      break
+
+    elsif number > value
+       number_array << letter
+       number = (number - value)
+       if number == 1
+        number_array << "i"
+        counter = 0
+      end
+       break
     end
   end
+end
+
   result = number_array.join("")
-  result	
-
-
-
-	# broken_number = number.to_s.split("")
-	# numeral = []
-	# if roman_conversion.has_key?(number)
-	# 	roman_conversion[number]
-	# else number > 10 && broken_number[1] == 0
-	# 	until broken_number[0].to_i == 0
-	# 		roman_conversion.each do |integer, roman|
-	# 		if broken_number[0].to_i >= integer
-	# 			numeral.push(roman)
-	# 			broken_number[0].to_i = broken_number[0]to_i - integer
-	# 		end
-	# 	end
-	# end
-	# else
-	# 	until number == 0
-	# 	roman_conversion.each do |integer, roman|
-	# 		if number >= integer
-	# 			numeral.push(roman)
-	# 			number = number - integer
-	# 		end
-	# 	end
-	# end
-	# puts numeral.to_s
+  result
 
 end
-puts roman_numeral(20)
-roman_numeral(20)
+puts roman_numeral(3941)
+# roman_numeral(20)
 
